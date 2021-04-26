62cba023ed41dec684280a9c81e6d89ab2cfe316
Devin MacGillivray <devhmac@gmail.com>
2021-04-26 18:27:43 +0000

    adds missing end

--- game_class.rb
+++ game_class.rb
@@ -1,9 +1,8 @@
-class GameLoop
+class Game
+  attr_accessor :game_over  
+  attr_reader :current_player
 
-attr_accessor :game_over
-attr_reader :current_player
-
-  initialize
+  def initialize
     @game_over = false
     @current_player = nil
     @current_question = nil
@@ -14,13 +13,19 @@ attr_reader :current_player
     end
 
     def set_current_player(p1,p2)
-      current_player == p1 ? @current_player = p2 : current_player = p1
+      @current_player == p1 ? @current_player = p2 : @current_player = p1
+    end
+
+    def check_current_player_life
+      if(@current_player.life == 0)
+        @game_over = false
+      end
     end
 
     def gameplay_loop
-
       while self.game_over == false
 
+      end
     end
 
 end
