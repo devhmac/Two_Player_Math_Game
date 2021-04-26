22430cee42e7c8b93b6c8d344ac237f1215759a1
Devin MacGillivray <devhmac@gmail.com>
2021-04-26 19:07:26 +0000

    cli print formatting

--- game_class.rb
+++ game_class.rb
@@ -18,21 +18,22 @@ class Game
 
     def check_game_status(p1,p2)
       if p1.life == 0 || p2.life == 0
-        puts "----- Game Over -----"
+        puts "\n----- Game Over -----"
         puts p1.life == 0 ? "Player 2 wins with a score of #{p2.life}/3" : "Player 1 wins with a score of #{p1.life}/3" 
         @game_over = true
       end
     end
 
     def start_game (player1,player2,questions)
-      while game.game_over == false
+      while self.game_over == false
         self.set_current_player(player1,player2)
       
         puts "\n Player 1: #{player1.life}/3 vs Player 2: #{player2.life}/3 "
-        puts "---- start turn ----"
+        puts "----- start turn -----"
         current_q = questions.gen_question
       
         puts "Player #{self.current_player.id}: what does #{current_q} equal."
+        print "> "
         answer = $stdin.gets.chomp
       
         if questions.eval_answer?(current_q,answer.to_i)
