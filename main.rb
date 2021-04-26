c673ca78e0dadf47298a22ecef354a60ed40a43f
Devin MacGillivray <devhmac@gmail.com>
2021-04-26 18:28:09 +0000

    requires working correctly

---
+++ main.rb
@@ -0,0 +1,19 @@
+require_relative './game_class'
+require_relative './questions_class'
+require_relative './player_class'
+
+player1 = Player.new(1)
+player2 = Player.new(2)
+
+
+
+
+# i = 0
+# while i < 5
+# puts "---- start turn ----"
+
+
+
+
+# i += 1
+# end
